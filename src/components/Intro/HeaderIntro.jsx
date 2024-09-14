cbad3803a85cefda4a2a057ae0370e83f713afb8
Trần Nguyễn Khánh Duy <71444587+santaduytran@users.noreply.github.com>
2024-09-14 21:14:30 +0700

    Update HeaderIntro.jsx

--- src/components/Intro/HeaderIntro.jsx
+++ src/components/Intro/HeaderIntro.jsx
@@ -12,11 +12,7 @@ const HeaderIntro = () => {
           <div className="col image-content">
             <h2>Weathering With You</h2>
             <p>
-              Weathering with You is a Japanese animated romantic fantasy film.
-              It follows a 16-year-old high school boy, Hodaka Morishima, who
-              runs away from his troubled rural home to Tokyo, and later
-              befriends Hina Amano, an orphaned girl who has the ability to
-              control the weather.
+              Weathering with You is a Japanese animated romantic fantasy film. It follows a 16-year-old high school boy, Hodaka Morishima, who runs away from his troubled rural home to Tokyo, and later befriends Hina Amano, an orphaned girl who has the ability to control the weather.
             </p>
           </div>
         </div>
